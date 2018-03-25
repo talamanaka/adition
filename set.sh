0fec9da80eb4eee165439a3760b8e50fe77fb045
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-03-25 22:45:58 +0100

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script will test if you have given a leap year or not.
+
+echo "Type the username shodan.io , followed by [ENTER]:"
+read user
+sed -i "s/username+/$user/" 2sh0dan.py
+echo $user\n
+echo "Type the password shodan.io , followed by [ENTER]:"
+read pass
+sed -i "s/pass+/$pass/" 2sh0dan.py
+echo $pass\n
+
+
+
+
