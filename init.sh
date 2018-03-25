57fa1d14a1663e2f464cf80e40b7193abfe774bd
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-03-25 19:12:47 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+apt-get update &&  apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp python-pip -y
+
+sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
+
+
+pip install selenium
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz 
+tar -xvf geckodriver-v0.20.0-linux64.tar.gz
+chmod +x geckodriver
+cp geckodriver /usr/bin/geckodriver
