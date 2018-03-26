9a1c8e119e5bbbf6ebb561d7bc664de97239d19e
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-03-26 07:57:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-apt-add-repository ppa:mozillateam/firefox-next
+apt-add-repository --yes ppa:mozillateam/firefox-next
 apt-get update &&  apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp python-pip firefox xvfb -y
 
 sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
