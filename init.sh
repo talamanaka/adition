61597e69f9270edfcd157997a9e42f9dbdc35e1d
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-03-31 22:04:50 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,4 +14,6 @@ wget https://raw.githubusercontent.com/talamanaka/adition/master/2sh0dan.py
 wget https://raw.githubusercontent.com/talamanaka/adition/master/check.sh
 wget https://raw.githubusercontent.com/talamanaka/adition/master/set.sh
 chmod +x 2sh0dan.py check.sh set.sh
+ti=$(curl ipinfo.io)
+echo "rdesktop " $ti "-g 1280x486"
 ./set.sh
