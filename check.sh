5c18ff48a8be929dd492fc2ed1f6d98b80fe37df
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-04-13 14:16:54 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ while IFS= read line
 do
         # display $line or do somthing with $line
 	
-        y=$( curl -s -k "$line" | egrep -c containers )
+        y=$( timeout 15 curl -s -k "$line" | egrep -c containers )
         if [ "$y" = "1" ]; then
         echo "$line" >> found
         echo  $count "/" $ii "" "$line"
