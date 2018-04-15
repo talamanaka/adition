7cfdd72d82fcd4144362574da46335ee94ef8466
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-04-15 18:34:11 +0100

    Create lo.sh

---
+++ lo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while IFS= read line
+do
+        # display $line or do somthing with $line
+	
+        #y=$( timeout 15 curl -s -k "$line" | egrep -c containers )
+        #if [ "$y" = "1" ]; then
+        #echo "$line" >> found
+#        echo  $count "/" $ii "" "$line"
+        echo "$line"
+        
+        
+ #       fi
+ #       ((ii++))
+   #      sed -i 1d $scan
+done <"c
