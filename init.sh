b04d746069f7b71420b3dc8beed179c53174959f
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-04-15 18:34:57 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,9 +15,10 @@ wget https://raw.githubusercontent.com/talamanaka/adition/master/check.sh
 wget https://raw.githubusercontent.com/talamanaka/adition/master/set.sh
 wget https://raw.githubusercontent.com/talamanaka/adition/master/q1.py
 wget https://raw.githubusercontent.com/talamanaka/adition/master/c
+wget https://raw.githubusercontent.com/talamanaka/adition/master/lo.sh
 
 echo " /usr/bin/python 2sh0dan.py $1 $2 $3"  > 2
-chmod +x 2sh0dan.py check.sh set.sh 2
+chmod +x 2sh0dan.py check.sh set.sh 2 lo.sh
 cp 2 /usr/bin/5
 cp 2sh0dan.py /usr/bin/2sh0dan.py
 ti=$(curl ipinfo.io/ip)
