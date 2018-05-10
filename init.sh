99f40d4b64ec94a32c7f68ff40f257c74ae8fea2
talamanaka <36954803+talamanaka@users.noreply.github.com>
2018-05-10 17:55:05 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 wget https://gist.githubusercontent.com/rohitrawat/60a04e6ebe4a9ec1203eac3a11d4afc1/raw/fcdfde2ab57e455ba9b37077abf85a81c504a4a9/sources.list && rm /etc/apt/sources.list && mv sources.list /etc/apt/sources.list
+echo -e "deb http://us.archive.ubuntu.com/ubuntu/ xenial main restricted\ndeb http://us.archive.ubuntu.com/ubuntu/ xenial-updates main restricted\ndeb http://us.archive.ubuntu.com/ubuntu/ xenial universe\ndeb http://us.archive.ubuntu.com/ubuntu/ xenial-updates universe\ndeb http://us.archive.ubuntu.com/ubuntu/ xenial multiverse\ndeb http://us.archive.ubuntu.com/ubuntu/ xenial-updates multiverse\ndeb http://us.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse\ndeb http://security.ubuntu.com/ubuntu xenial-security universe\ndeb http://security.ubuntu.com/ubuntu xenial-security multiverse\n" > sources.list
+
 passwd root 
 apt-add-repository --yes ppa:mozillateam/firefox-next
 apt-add-repository --yes ppa:ubuntu-mate-dev/ppa
 apt-add-repository --yes ppa:ubuntu-mate-dev/trusty-mate
-
-apt-get update &&  apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp python-pip firefox xvfb -y
+apt-get update
+apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp python-pip firefox xvfb -y
 
 sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
 
